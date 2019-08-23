084392754be7965178103359d91343bdd238c33f
ela_pg <mpgonzales14@gmail.com>
2019-08-23 11:10:44 -0700

    finsih sprint challange javascript

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,7 +6,9 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume (argument1, argument2, cb){
+  return cb(argument1, argument2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +16,21 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add (a, b) {
+  return a + b;
+}
+
+function multiply (x, y) {
+  return x * y;
+}
+ function greeting (first, last){
+ return Hello `${first.name}  ${last.name} nice to meet you!`;
+ }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply));  // 160
+ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,7 +38,8 @@
 // Explain in your own words why nestedfunction can access the variable internal.
 
 // Explanation: 
-
+ because it's inside the function and it seems like the nestedFunction could only go 
+ down it's like the parent function.
 
 const external = "I'm outside the function";
 
